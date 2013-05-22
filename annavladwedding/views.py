17e117e7c7202f200d2c10b08a8f521e86268294
Jon Eyolfson <jon@eyolfson.com>
2013-05-22 14:13:24 -0400

    Added simple RSVP form

--- annavladwedding/views.py
+++ annavladwedding/views.py
@@ -1,4 +1,15 @@
-from django.shortcuts import render
+from django import forms
+from django.shortcuts import redirect, render
+
+class RSVPForm(forms.Form):
+    confirmation_code = forms.CharField(max_length=4, widget=forms.TextInput(
+        attrs={'class': 'input-mini'}))
 
 def home(request):
-    return render(request, 'annavladwedding/home.html')
+    if request.method == 'POST':
+        form = RSVPForm(request.POST)
+        if form.is_valid():
+            return redirect('annavladwedding.views.home')
+    else:
+        form = RSVPForm()
+    return render(request, 'annavladwedding/home.html', {'form': form})
