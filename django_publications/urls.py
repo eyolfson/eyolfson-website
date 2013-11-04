ab86ea226cdbc2d855056d5d6be97753381ca76c
Jon Eyolfson <jon@eyolfson.com>
2013-11-03 22:29:28 -0500

    Fixed slug regular expression

--- django_publications/urls.py
+++ django_publications/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from django_publications import views
 
 urlpatterns = patterns('',
-   url(r'^(?P<slug>\w+)/bibtex/$', views.bibtex, name='bibtex'),
+   url(r'^(?P<slug>[-a-zA-Z0-9]+)/bibtex/$', views.bibtex, name='bibtex'),
 )
